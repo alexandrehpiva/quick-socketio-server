87719df451b2d7b1e5e798234ec81db17f89bda3
Alexandre Piva <alexandrehpiva@gmail.com>
2021-08-09 21:08:58 -0300

    Example of return the response directly

--- app.py
+++ app.py
@@ -11,8 +11,16 @@ def connect(sid, environment):
 def disconnect(sid):
   print(sid, 'disconnected')
 
+# Emit an event after sum
+@sio.event
+def sum_to_sum_result(sid, data):
+  print(sid, data)
+  result = data['numbers'][0] + data['numbers'][1]
+  sio.emit('sum_result', { 'result': result }, to=sid)
+
+# Simply return the result after sum
 @sio.event
 def sum(sid, data):
   print(sid, data)
   result = data['numbers'][0] + data['numbers'][1]
-  sio.emit('sum_result', { 'result': result }, to=sid)
+  return {'result': result}
--- app_async.py
+++ app_async.py
@@ -11,8 +11,16 @@ def connect(sid, environment):
 def disconnect(sid):
   print(sid, 'disconnected')
 
+# Emit an event after sum
+@sio.event
+async def sum_to_sum_result(sid, data):
+  print(sid, data)
+  result = data['numbers'][0] + data['numbers'][1]
+  await sio.emit('sum_result', { 'result': result }, to=sid)
+
+# Simply return the result after sum
 @sio.event
 async def sum(sid, data):
   print(sid, data)
   result = data['numbers'][0] + data['numbers'][1]
-  await sio.emit('sum_result', { 'result': result }, to=sid)
+  return {'result': result}
